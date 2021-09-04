5e0fe9d8c5c034507cbb88dc9ca3492bac6cdfd0
Salavat <salavatmerhatimovic@gmail.com>
2021-09-04 21:51:52 +0300

    all cors

--- backend/app.js
+++ backend/app.js
@@ -36,12 +36,11 @@ app.use(helmet());
 app.use(limiter);
 app.use(cookieParser());
 app.use(bodyParser.json());
-// https://slt116.nomoredomains.club
-app.use(cors({
-  origin: 'http://localhost:3000',
-  credentials: true,
-}));
-// app.use('*', cors());
+// app.use(cors({
+//   origin: 'https://slt116.nomoredomains.club',
+//   credentials: true,
+// }));
+app.use('*', cors());
 app.use(requestLogger); // подключаем логгер запросов
 
 app.post('/signup', validateSignUp, createUser);
