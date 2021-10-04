30cf6f64d3354e4b3503d0b0488884b0a9894fec
Salavat <salavatmerhatimovic@gmail.com>
2021-10-04 18:52:13 +0300

    1

--- backend/errors/ConflictErrors.js
+++
@@ -1,8 +0,0 @@
-class ConflictErrors extends Error {
-  constructor(message) {
-    super(message);
-    this.statusCode = 409;
-  }
-}
-
-module.exports = ConflictErrors;
--- backend/errors/email-conflict-err.js
+++
@@ -1,8 +0,0 @@
-class EmailConflictError extends Error {
-  constructor(message) {
-    super(message);
-    this.statusCode = 409;
-  }
-}
-
-module.exports = EmailConflictError;
