27fb52844879b9ddfbf5b0f24d6c1e6ead023e39
Salavat <salavatmerhatimovic@gmail.com>
2021-10-05 22:05:35 +0300

    1

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,15 +1,15 @@
 const jwt = require('jsonwebtoken');
 const AuthorizationError = require('../errors/authorization-err');
 
-// const { JWT_SECRET, NODE_ENV } = process.env;
+const { JWT_SECRET, NODE_ENV } = process.env;
 
 module.exports = (req, res, next) => {
   const token = (req.cookies.jwt);
 
   let payload;
-  //  NODE_ENV === 'production' ? JWT_SECRET : 'super-strong-secret-test'
+
   try {
-    payload = jwt.verify(token);
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'super-strong-secret-test');
   } catch (err) {
     next(new AuthorizationError('Пожалуйста, авторизуйтесь'));
   }
